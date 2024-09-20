c9697652f7ddddebf7fb7e58f1c8c3dd1d6bc962
patchker <paczker@icloud.com>
2024-09-20 11:39:28 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// src/App.tsx
+
+function App() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+            <h1 className="text-5xl font-semibold text-gray-900">
+                Initial project
+            </h1>
+            <h1 className="text-2xl font-bold text-blue-600 mt-3">
+                [Vite + React + Tailwind]
+            </h1>
+        </div>
+    )
+}
+
+export default App
